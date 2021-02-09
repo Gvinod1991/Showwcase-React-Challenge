4991c691c849f66d38d07cb6a27c5999991cf3c7
Godti Vinod <venkat.godti3@gmail.com>
2021-02-09 12:48:36 +0530

    Education showcase entry screen implementaion

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,30 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+import Button from '../components/Button';
+import Input from "../components/Input";
+
 export default function Home() {
+  const [userName, setUserName] = useState<string>("");
   return (
-    <div>
-      <h1>Next.Ts App</h1>
-    </div>
+    <HomeWrapper>
+      <Label>Type of your name and click "Enter" to begin</Label>
+      <Input onChange={(e) => setUserName(e.currentTarget.value)}
+        placeholder={"Your Name"} value={userName} />
+      <Button onClick={() => { }}>Enter</Button>
+    </HomeWrapper>
   )
 }
+
+export const HomeWrapper = styled.div`
+  padding: 2rem 0;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  font-size: 1.25rem;
+`;
+export const Label = styled.p`
+  font-size:1rem;
+  color:${({ theme }) => (theme.colors.text)}
+`
